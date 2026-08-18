438d1a85028e03d78ac41ac13e1aa77a01b17d0d
Vlabimer Kharitonov <maverik2156@gmail.com>
2026-08-18 11:14:40 +0300

    1

--- Age.py
+++ Age.py
@@ -1,9 +1,9 @@
-year_of_birth = int(input("Enter year of birth: "))
-current_year = int(input("Enter current year: "))
-if current_year < year_of_birth:
+a = int(input("Enter year of birth: "))
+b = int(input("Enter current year: "))
+if b < a:
     print("Error: Current year can't be less than year of birth.")
 else:
-    age = int(current_year - year_of_birth)
+    age = int(b - a)
     if age < 0:
         print("Error: Year of birth can't be greater than the current year.")
     else:
