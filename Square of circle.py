2c0571b254595164a77b83d0c6a072431a5a1fa9
Vlabimer Kharitonov <maverik2156@gmail.com>
2026-08-18 12:55:38 +0300

    Сircle square

---
+++ Square of circle.py
@@ -0,0 +1,11 @@
+# Function to calculate the square of a radius
+import math
+
+def sq(r):
+    return r * r
+
+r = float(input("Enter the radius of the circle in cm: "))
+
+sq_circle = math.pi * sq(r)
+
+print(f"The area of the circle is {sq_circle:.2f} cm².") 
