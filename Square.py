8048c7232057f05e16f8ae593e19119005bcbd5c
Vlabimer Kharitonov <maverik2156@gmail.com>
2026-08-18 12:37:57 +0300

    V 1.0

--- Square.py
+++ Square.py
@@ -1,12 +1,19 @@
+# Функция для вычисления площади четырехугольника
 def square(length, width):
     return length * width
 
+# Функция для вычисления периметра четырехугольника
 def perimeter(length, width):
     return 2 * (length + width)
 
+# Ввод длины и ширины четырехугольника
 a = float(input("Enter the Length in cm: "))
 b = float(input("Enter the Width in cm: "))
-sqresult = square(a, b)
-perimeter_result = perimeter(a, b)
-print(f"The square {sqresult} cm².")
-print(f"The perimeter {perimeter_result} cm.")
+
+# Вычисление площади и периметра
+sq_result = square(a, b)
+per_result = perimeter(a, b)
+
+# Вывод результатов
+print(f"The square {sq_result} cm².")
+print(f"The perimeter {per_result} cm.")
