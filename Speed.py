d05f4501f546a29684312e88ec375b31fe13b103
Vlabimer Kharitonov <maverik2156@gmail.com>
2026-08-19 14:45:25 +0300

    t == 0

--- Speed.py
+++ Speed.py
@@ -4,4 +4,7 @@ def V(S, t):
 S = float(input("Enter the distance in km: "))
 t = float(input("Enter the time in hours: "))
 
-print(f"The speed is {V(S, t):.2f} km/h.")
+if t == 0:
+    print("Time can't be zero. Please enter a valid time.")
+else:
+    print(f"The speed is {V(S, t):.2f} km/h.")
